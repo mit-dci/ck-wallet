a34161628b62fcd1ecae1f0de79b08ef9c4d774c
bsnowden3 <bsnowden@mit.edu>
2018-06-07 16:59:23 -0400

    add ForgotPassword detail text button

--- ck-wallet/src/views/SignInViewComponent.js
+++ ck-wallet/src/views/SignInViewComponent.js
@@ -1,20 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import SignInValidationComponent from '../components/SignInValidationComponent';
-import RegisterAccountViewComponent from  './RegisterAccountComponentView';
+import { StyleSheet, Text, TextInput, View, Dimensions } from 'react-native';
+import ButtonWithActionComponent from '../components/ButtonWithActionComponent';
 import { APP_BACKGROUND_COLOR, DETAIL_TEXT_COLOR, FORM_FIELD_BACKGROUND_COLOR } from '../constants/styles.js';
 
 export default class SignInViewComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { emailText: '', passwordText: '' };
+  }
+
+  signInHandler() {
+    console.log("What")
+  }
+
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.headerStyle}> Sign In </Text>
         <Text style={styles.descriptionStyle}> Sign In to your K320 account </Text>
-        <SignInValidationComponent />
+        <TextInput
+          style={{marginTop: 25, height: 40, width: 275, backgroundColor: FORM_FIELD_BACKGROUND_COLOR, borderRadius: 5, color: DETAIL_TEXT_COLOR}}
+          onChangeText={(emailText) => this.setState({emailText})}
+          value={this.state.emailText}
+          placeholder="example@example.com"
+          placeholderTextColor={DETAIL_TEXT_COLOR}
+        />
+
+        <TextInput
+        style={{marginTop: 15, marginBottom: 4, height: 40, width: 275, backgroundColor: FORM_FIELD_BACKGROUND_COLOR, borderRadius: 5, color: DETAIL_TEXT_COLOR}}
+        onChangeText={(passwordText) => this.setState({passwordText})}
+        value={this.state.passwordText}
+        secureTextEntry
+        placeholder="your password"
+        placeholderTextColor={DETAIL_TEXT_COLOR}
+        />
+
+        <View style={styles.forgotPasswordContainer}>
+          <Text onPress={() => this.props.navigation.navigate("ForgotPassword")} style={{color: DETAIL_TEXT_COLOR, marginBottom: 20}}> Forgot Password? </Text>
+        </View>
+
+        <ButtonWithActionComponent onPressHandler={this.signInHandler} text="Sign In" source={require('../assets/right_arrow_3.png')}/>
+
         <View style={{marginTop: 50}}>
           <Text style={{color: DETAIL_TEXT_COLOR}}> Don't have an account?
-           <Text onPress={this.props.onPressHandler} style={styles.registerAcccountTextStyle}> Register </Text>
+           <Text> </Text>
+           <Text onPress={() => this.props.navigation.navigate('Register')} style={styles.registerAcccountTextStyle}> Register </Text>
           </Text>
         </View>
       </View>
@@ -26,8 +58,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    height: 350,
-    width: 670,
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
     backgroundColor: APP_BACKGROUND_COLOR,
     alignItems: 'center',
     justifyContent: 'center',
@@ -36,6 +68,11 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
     marginTop: 20
+  },
+  forgotPasswordContainer: {
+    flex: 0,
+    width: 275,
+    alignItems: 'flex-end',
   },
   descriptionStyle: {
     color: 'white',
